dd05ce5a4b56e0c8a7d389cfe5647770ee967cc8
sonu kumar <snu0929@gmail.com>
2023-01-20 20:31:42 +0530

    day-4 task completed

---
+++ OTP.js
@@ -0,0 +1,28 @@
+let codes=document.querySelectorAll(".code")
+
+    codes[0].focus()
+    codes.forEach((code,idx)=>{
+   code.addEventListener("keydown",(e)=>{
+    if(e.key>=0 &&e.key<=9){
+        codes[idx].value=""
+        setTimeout(()=>codes[idx+1].focus(), 10)
+    }else if(e.key==="Backspace"){
+        setTimeout(()=>codes[idx-1].focus(), 10)
+    }
+   })
+    })
+
+   
+let popup=document.getElementById("popup")
+function openPopup(){
+    let body=document.querySelector(".container")
+    body.style.opacity="0.5"
+    popup.classList.add("open-popup")
+  
+    
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup")
+    window.location.href="index.html"
+}
